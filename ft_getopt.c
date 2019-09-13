b7c000e310369d8dccfa48361f85ab22ce6c2ff2
Gormlaith Melisandre <gmelisan@student.21-school.ru>
2019-09-13 18:54:13 +0300

    seems good

--- ft_getopt.c
+++ ft_getopt.c
@@ -6,7 +6,7 @@
 /*   By: gmelisan <gmelisan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/09 18:40:50 by gmelisan          #+#    #+#             */
-/*   Updated: 2019/09/09 19:50:59 by gmelisan         ###   ########.fr       */
+/*   Updated: 2019/09/13 18:43:20 by gmelisan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,59 +16,66 @@ char		*g_optarg;
 int			g_optind = 1;
 int			g_optopt;
 
-static int	found(int argc, char * const argv[], char *f, int *pos)
+static void	update_indpos(char *const argv[], int *pos)
 {
+	if (argv[g_optind][*pos + 1] == '\0')
+	{
+		g_optind++;
+		*pos = 1;
+	}
+	else
+		*pos += 1;
+}
+
+static int	found(int argc, char *const argv[], char *f, int *pos)
+{
+	g_optarg = NULL;
 	if (*(f + 1) == ':')
 	{
 		if (argv[g_optind][*pos + 1] == '\0')
 		{
 			g_optarg = argv[g_optind + 1];
-			g_optind += 1;
-			if (argv[g_optind] && *argv[g_optind] != '-') /* todo check */
-				return ('?');
-			g_optind += 1;
-			return (g_optind > argc ? -1 : g_optopt);
+			g_optind += 2;
+			*pos = 1;
+			return (g_optind > argc ? '?' : g_optopt);
 		}
 		else
 		{
 			g_optarg = &argv[g_optind][*pos + 1];
 			g_optind += 1;
+			*pos = 1;
 			return (g_optopt);
 		}
 	}
-	if (argv[g_optind][*pos + 1] == '\0')
-	{
-		g_optind += 1;
-		*pos = 1;
-	}
-	else
-		*pos += 1;
+	update_indpos(argv, pos);
 	return (g_optopt);
 }
 
-int			ft_getopt(int argc, char * const argv[], const char *optstring)
+static int	not_found(char *const argv[], int *pos)
+{
+	update_indpos(argv, pos);
+	return ('?');
+}
+
+int			ft_getopt(int argc, char *const argv[], const char *optstring)
 {
 	static int	pos = 1;
 	char		*f;
 
-	if (!argv[g_optind] || g_optind > argc - 1
+	if (!optstring || !argv[g_optind] || g_optind > argc - 1
 		|| *argv[g_optind] != '-' || ft_strequ(argv[g_optind], "-"))
 		return (-1);
-	if (ft_strequ(argv[g_optind], "--"))
+	if ((g_optopt = argv[g_optind][pos]))
 	{
-		g_optind++;
-		return (-1);
-	}
-	while ((g_optopt = argv[g_optind][pos]))
-	{
+		if (ft_strequ(argv[g_optind], "--"))
+		{
+			g_optind++;
+			return (-1);
+		}
 		if ((f = ft_strchr(optstring, g_optopt)))
 			return (found(argc, argv, f, &pos));
 		else
-		{
-			g_optind++;
-			pos = 1;
-			return ('?');
-		}
+			return (not_found(argv, &pos));
 	}
 	return (-1);
 }
--- main.c
+++ main.c
@@ -6,36 +6,40 @@
 /*   By: gmelisan <gmelisan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/09 19:39:33 by gmelisan          #+#    #+#             */
-/*   Updated: 2019/09/09 19:44:12 by gmelisan         ###   ########.fr       */
+/*   Updated: 2019/09/13 18:32:33 by gmelisan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "ft_getopt.h"
 
-int		main(int argc, char *argv[])
+typedef int (*pfunc)(int, char *const *, const char *);
+
+void	test(FILE *fp, int argc, char *argv[], pfunc f, char **_optarg, int *_optind, int *_optopt)
 {
 	int c;
 
-	while ((c = ft_getopt(argc, argv, ":abcd:e")) != -1)
+	while ((c = f(argc, argv, "abc:def")) != -1)
 	{
-		if (c == 'a')
-		{
-			printf("Have a\n");
-		}
-		else if (c == 'b')
-			printf("Have b\n");
-		else if (c == 'c')
-			printf("Have c\n");
-		else if (c == 'd')
-			printf("Have d\n");
-		else if (c == 'e')
-			printf("Have e\n");
-		else
-			printf("Unknown\n");
-		printf("getopt returned %d (%c)\n", c, c);
-		printf("optarg = %s\n", g_optarg);
-		printf("optind = %d\n", g_optind);
-		printf("optopt = %d\n", g_optopt);
+		fprintf(fp, "- getopt returned %d (%c)\n", c, c);
+		fprintf(fp, "optarg = %s\n", *_optarg);
+		fprintf(fp, "optind = %d\n", *_optind);
+		fprintf(fp, "optopt = %d\n", *_optopt);
 	}
+	fprintf(fp, "\ngetopt returned %d (%c)\n", c, c);
+	fprintf(fp, "optarg = %s\n", *_optarg);
+	fprintf(fp, "optind = %d\n", *_optind);
+	fprintf(fp, "optopt = %d (%c)\n", *_optopt, *_optopt);
+}
+
+int		main(int argc, char *argv[])
+{
+	FILE *f1;
+	FILE *f2;
+
+	f1 = fopen("system.txt", "w+");
+	f2 = fopen("my.txt", "w+");
+	test(f1, argc, argv, getopt, &optarg, &optind, &optopt);
+	test(f2, argc, argv, ft_getopt, &g_optarg, &g_optind, &g_optopt);
 	return (0);
 }
